0df22a4e1fcdff02dd9e2814e0c7290d1e8c7417
Amalia <amalialorenaferariu@gmail.com>
2021-10-22 19:18:06 +0200

    hide menu under 1000px

--- main.js
+++ main.js
@@ -2,23 +2,19 @@
 
 // Hamburger menu: mostrare / nascondere il menu principale
 
-
-//Quando clico sul menu hamburgher, faccio comparire il menu nascosto se il dispositivo ha la width minore di 1000px
-
+//Quando clico sul menu hamburgher, faccio comparire il menu nascosto 
 const hamburger = document.querySelector(".header-right a i");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const x = document.querySelector(".hamburger-menu a.close")
-// let mql = window.matchMedia('(min-width: 1000px)');
+let mq = window.matchMedia('(max-width: 1000px)');
 
 
 hamburger.addEventListener('click',
 
     function() {
-       
+
         hamburgerMenu.style.display = "block" ;
-
     }
-
 );
 
 //quando clico sulla x, faccio scomparire il menu nascosto
@@ -33,7 +29,27 @@ x.addEventListener('click',
 
 );
 
-
-
-
 //il menu nascosto scompare se il dispositivo ha la width maggiore di 1000px
+
+window.addEventListener("resize", 
+    function(){
+       
+        if (window.innerWidth > 1000){
+            hamburgerMenu.style.display = "none" ;
+        }
+
+        // oppure:
+        
+        // if (mq.matches == false) {
+        //     hamburgerMenu.style.display = "none" ;
+        // }
+
+  });
+
+
+
+
+
+
+
+ 
